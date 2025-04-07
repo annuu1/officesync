e0141c2c99d28d4519e31f1555c0722bb6d0ebb8
annuu1 <mannusng876@gmail.com>
2025-04-07 15:46:54 +0530

    add Tailwind CSS integration and create Dashboard component; set up routing in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import './index.css';
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+    </BrowserRouter>
   );
 };
 
